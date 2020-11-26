d816a7c0be371bbdf1b1a4b756b39c411fa74a76
Andrew Friend <andrewfriend@gmail.com>
2020-11-26 09:09:13 +0000

    edits to js

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ var morgan = require('morgan');
 var app = express();
 var port = 80;
 
+var messages = [];
+var prevMessages;
+
 app.use(morgan('combined'));
 
 app.use(bodyParser({extended:false}));
 app.use(express.static('pages'));
 
-app.use('/test', (req,res) => {
+app.get('/test', (req,res) => {
 	res.send('hello, your test has worked');
 
+})
+
+app.get('/', (req,res) => {
+	res.sendFile(__dirname + "/pages/index.html");
 })
 
 app.listen(port, () => {
